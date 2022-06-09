69df3dde266d70723d51ad102586d59b4874a5fd
Pamela <pamela.szymaniec@gmail.com>
2022-06-09 17:47:28 +0200

    Setting up array with cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/cat-1.png" },
+  { src: "/img/cat-2.png" },
+  { src: "/img/cat-3.png" },
+  { src: "/img/cat-4.png" },
+  { src: "/img/cat-5.png" },
+  { src: "/img/cat-6.png" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.floor(Math.random() * 10000000) }));
+    setCards(shuffledCards);
+    setTurns(0);
+    console.log(shuffledCards);
+  };
+
   return (
     <div className="App">
       <h1>Cats Memory</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
