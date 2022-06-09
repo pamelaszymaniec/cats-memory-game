b139f6ab592721df250bd8b90cefbd3be281e3e1
Pamela <pamela.szymaniec@gmail.com>
2022-06-09 19:14:57 +0200

    Flipping cards - animation and delay

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,12 @@
 import "./SingleCard.css";
 
-function SingleCard({ card, handleChoice }) {
+function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   };
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front"></img>
         <img
           className="back"
